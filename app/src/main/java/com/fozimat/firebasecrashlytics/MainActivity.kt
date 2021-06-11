b58ac79551eddd8d0ce035182b7dceaa32b8a428
Fozimat <fozimata@gmail.com>
2021-06-11 19:27:51 +0700

    Implementation Timber

--- app/src/main/java/com/fozimat/firebasecrashlytics/MainActivity.kt
+++ app/src/main/java/com/fozimat/firebasecrashlytics/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fozimat.firebasecrashlytics.databinding.ActivityMainBinding
 import com.google.firebase.crashlytics.FirebaseCrashlytics
+import timber.log.Timber
 import java.lang.RuntimeException
 
 class MainActivity : AppCompatActivity() {
@@ -15,10 +16,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        Timber.d("Test debugging")
+
         binding.btnCrash.setOnClickListener {
             FirebaseCrashlytics.getInstance().log("Clicked on button")
             FirebaseCrashlytics.getInstance().setCustomKey("str_key", "some_data")
-            throw RuntimeException("Test Crash")
+            try {
+                throw RuntimeException("Test Crash")
+            } catch (e: Exception) {
+                Timber.e("Test non fatal exception")
+            }
         }
     }
 }
